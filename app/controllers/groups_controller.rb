6702be60d58c0203190e072d8c4918b28a750d97
dongmy54 <615760433@qq.com>
2017-03-29 16:43:11 +0800

    user before_action to find_group_and_check_permission

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
     before_action :authenticate_user! ,only: [:new, :create, :update, :edit, :destroy]
-
+    before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
     def index
     	@groups = Group.all
     end
@@ -25,42 +25,33 @@ class GroupsController < ApplicationController
     end
 
     def edit
-    	@group = Group.find(params[:id])
-
-        if current_user != @group.user
-            redirect_to root_path, alert: "你不是群的拥有者，不能进行编辑"
-        end
-        @group.title = "ruby"
-        @group.save
     end
 
     def update
-         if current_user != @group.user
-            redirect_to root_path, alert: "你不是群的拥有者，不能进行修改"
-         else
-    	 @group = Group.find(params[:id])
     	 if @group.update(group_params)
     	    redirect_to groups_path, notice: "修改成功"
          else
          render :edit
          end
-         end
     end
 
     def destroy
-        if current_user != @group.user
-            redirect_to root_path, alert: "你不是群的拥有者，不能进行删除"
-        else
-    	@group = Group.find(params[:id])
     	@group.destroy
-    	redirect_to groups_path
-        flash[:alert] = "此群已被你删除"  
-        end     
+        redirect_to groups_path, alert: "Group deleted"
+          
     end
 
     private
 
     def group_params
     	params.require(:group).permit(:title, :description)
+    end
+
+    def find_group_and_check_permission
+        @group = Group.find(params[:id])
+        if current_user != @group.user
+            redirect_to root_path, alerts: "You have no permission"
+        end
+        
     end
 end
