3e2e48e0742f6997a8aaaa68c69f2511bb45ff42
dongmy54 <615760433@qq.com>
2017-03-29 11:41:52 +0800

    implement groups#create

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,4 +7,19 @@ class GroupsController < ApplicationController
     	@group = Group.new
     end
 
+    def create
+    	@group = Group.new(group_params)
+    	if @group.save
+    		redirect_to groups_path
+    		flash[:notice] = "群创建成功！"
+    	else
+    		render :new
+    	end
+    end
+
+    private
+
+    def group_params
+    	params.require(:group).permit(:title, :description)
+    end
 end
