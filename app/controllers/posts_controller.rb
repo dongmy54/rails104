e0e7e46114e8d998fcdd7e4ba113a2eebbc9899d
dongmy54 <615760433@qq.com>
2017-03-30 11:55:50 +0800

    implement my posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,28 @@ class PostsController < ApplicationController
 	   end
 	end
 
+	def edit
+		@post = Post.find(params[:id])
+		@group = Group.find(params[:group_id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+        
+	 if	@post.update(post_params)
+		flash[:notice] = "post修改成功"
+		redirect_to account_posts_path
+     else
+		render :edit
+	 end
+	 end
+
+	 def destroy
+	 	@post = Post.find(params[:id])
+	 	@post.destroy
+	 	redirect_to account_posts_path
+	 end
+
 	   private
 
 	   def post_params
