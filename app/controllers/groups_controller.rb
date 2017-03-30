00a1771603a055cd32773ec5b6c38e62a7700884
dongmy54 <615760433@qq.com>
2017-03-30 10:20:08 +0800

    join & quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -42,6 +42,32 @@ class GroupsController < ApplicationController
           
     end
 
+    def join
+        @group = Group.find(params[:id])
+
+        if !current_user.is_member_of?(@group)
+            current_user.join!(@group)
+            flash[:notice] = "加入本讨论群成功！"
+        else
+            flash[:warning] = "你已经是本讨论群的成员了！"
+        end
+
+        redirect_to group_path(@group)
+    end
+
+       def quit
+           @group = Group.find(params[:id])
+
+           if current_user.is_member_of?(@group)
+               current_user.quit!(@group)
+               flash[:notice] = "你已经退出本讨论群"
+           else
+             flash[:warning] = "你不是群成员，怎么退出 XD"
+           end
+
+           redirect_to group_path(@group)
+       end
+
     private
 
     def group_params
